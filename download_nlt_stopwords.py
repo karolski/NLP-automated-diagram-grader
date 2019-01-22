ce91dcbbee7195f1079304301e68413792d770ea
karolski <trickster.ks@gmail.com>
2019-01-22 13:31:53 +0100

    added numbers to scripts, separated some cripts renamed data to frequencies without changing it in notebooks yet

--- download_nlt_stopwords.py
+++ download_nlt_stopwords.py
@@ -8,5 +8,8 @@ except AttributeError:
     pass
 else:
     ssl._create_default_https_context = _create_unverified_https_context
-
+print("downloading stopwords...")
 nltk.download('stopwords')
+print("downloading punkt...")
+nltk.download('punkt')
+print("done")
